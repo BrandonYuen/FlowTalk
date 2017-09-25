13c88de06a7e528c08f1aaaee7f6a0333ea4586a
Brandon Yuen <brandon97@hotmail.com>
2017-09-25 21:21:58 +0200

    Signup, Login & Logout (with bcrypt)

---
+++ api/models/User.js
@@ -0,0 +1,68 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+	attributes: {
+		email: {
+			type: 'email',
+			required: true,
+			unique: true
+		},
+		password: {
+			type: 'string',
+			required: true
+		},
+		isAdmin: {
+			type: 'boolean',
+			defaultsTo: false
+		}
+	},
+
+
+	/**
+	* Create a new user using the provided inputs,
+	* but encrypt the password first.
+	*
+	* @param  {Object}   inputs
+	*                     • name     {String}
+	*                     • email    {String}
+	*                     • password {String}
+	* @param  {Function} cb
+	*/
+
+	signup: function (inputs, cb) {
+		// Create a user
+		User.create({
+			name: inputs.name,
+			email: inputs.email,
+			password: require('bcrypt-nodejs').hashSync(inputs.password)
+		})
+		.exec(cb);
+	},
+
+
+
+	/**
+	* Check validness of a login using the provided inputs.
+	* But encrypt the password first.
+	*
+	* @param  {Object}   inputs
+	*                     • email    {String}
+	*                     • password {String}
+	* @param  {Function} cb
+	*/
+
+	attemptLogin: function (inputs, cb) {
+		// Create a user
+		User.findOne({
+			email: inputs.email,
+			password: require('bcrypt-nodejs').hashSync(inputs.password)
+		})
+		.exec(cb);
+	}
+};
