b50eb4b28e97997db59558da01b2b0d70c63fd48
brandonyuen <brandon97@hotmail.com>
2018-03-26 19:33:47 +0000

    Updated ejs vulnerability

--- config/models.js
+++ config/models.js
@@ -29,6 +29,6 @@ module.exports.models = {
   ***************************************************************************/
   // migrate: 'alter'
 
-    connection: 'mysqlXAMP',
+    connection: 'localDiskDb',
     migrate: 'alter'
 };
